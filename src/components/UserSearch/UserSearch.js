6a31658cf818feeb5311ed67df4795e8dc4a2aa8
Aditya Subramanian <aditarun2008@gmail.com>
2022-04-01 21:09:46 -0400

    #36 updating side bar data according to #38

--- src/components/UserSearch/UserSearch.js
+++ src/components/UserSearch/UserSearch.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Search } from "@chatscope/chat-ui-kit-react";
 import * as api from "../../mocks/api/WazzupServerLib.js";
 import * as _ from "lodash";
 
-const UserSearch = () => {
+const UserSearch = (props) => {
+  const searchUser = () => {
+    let user = api.searchUser(value);
+    props.setUsers([]);
+    () => setValue("");
+  };
+
   const [value, setValue] = useState("Search ...");
   return <Search placeholder="Search..." value={value} onChange={(v) => setValue(v)} onClearClick={searchUser} />;
-};
-
-const searchUser = () => {
-  let user = api.searchUser(value);
-  let chat_id = api.initChat(_.get(user, "user_id"));
-  () => setValue("");
 };
